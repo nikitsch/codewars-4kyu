4982c148d704281646da93b45c3498dcba1d031f
nikitsch <nikitsch5@gmail.com>
2025-01-10 12:38:52 +0300

    feat: vitest

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,15 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 import prettier from 'eslint-config-prettier';
 import prettierPlugin from 'eslint-plugin-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    languageOptions: { globals: globals.browser },
-  },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
   prettier,
   {
     plugins: {
